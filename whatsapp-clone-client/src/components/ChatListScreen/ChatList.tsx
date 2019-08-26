5e941fffbc55f94e881d7cfd65e58144150ef2a0
Aman Sharma <amandeep.xml@gmail.com>
2019-08-26 00:34:38 -0400

    added node server and updated data fetch in the client

--- whatsapp-clone-client/src/components/ChatListScreen/ChatList.tsx
+++ whatsapp-clone-client/src/components/ChatListScreen/ChatList.tsx
@@ -1,31 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { List, ListItem } from "@material-ui/core";
 import moment from "moment";
+import axios from "axios";
 
-import { chats } from "../../database";
 import styles from "./ChatList.module.scss";
 
-const ChatList: React.FC = () => (
-  <div className="container">
-    <List>
-      {chats.map(item => {
-        return (
-          <ListItem key={item.id}>
-            <img src={item.picture} alt={item.name} />
-            <p className={styles.leftMargin}>{item.name}</p>
-            {item.lastMessage && (
-              <Fragment>
-                <p className={styles.leftMargin}>{item.lastMessage.content}</p>
-                <p className={styles.leftMargin}>
-                  {moment(item.lastMessage.createdAt).format("HH:mm")}
-                </p>
-              </Fragment>
-            )}
-          </ListItem>
-        );
-      })}
-    </List>
-  </div>
-);
+const ChatList: React.FC = () => {
+  const [chats, setChats] = useState(undefined);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/_chats")
+      .then(chats => setChats(chats.data));
+  }, []);
+
+  return (
+    <div className="container">
+      {!!chats && (
+        <List>
+          {chats.map(item => {
+            return (
+              <ListItem key={item.id}>
+                <img src={item.picture} alt={item.name} />
+                <p className={styles.leftMargin}>{item.name}</p>
+                {item.lastMessage && (
+                  <Fragment>
+                    <p className={styles.leftMargin}>
+                      {item.lastMessage.content}
+                    </p>
+                    <p className={styles.leftMargin}>
+                      {moment(item.lastMessage.createdAt).format("HH:mm")}
+                    </p>
+                  </Fragment>
+                )}
+              </ListItem>
+            );
+          })}
+        </List>
+      )}
+    </div>
+  );
+};
 
 export default ChatList;
--- whatsapp-clone-server/index.ts
+++ whatsapp-clone-server/index.ts
@@ -1,9 +1,40 @@
 import express from "express";
+import cors from "cors";
+import { chats } from "./data";
 
 const app = express();
+app.use(cors());
+
+// app.use(function(req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+
+// Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 app.get("/_ping", (req, res) => {
   res.send("pong");
+});
+
+app.get("/_chats", (req, res) => {
+  res.send(chats);
 });
 
 const port = process.env.PORT || 4000;
